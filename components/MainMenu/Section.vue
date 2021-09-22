51a709ccd0967c84528ec8a5324ac763ca4632af
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-23 03:04:21 +0800

    ✨ Added menu item section

---
+++ components/MainMenu/Section.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div
+      class="
+        px-3
+        py-2
+        text-gray-700
+        hover:bg-opacity-10
+        cursor-pointer
+        font-semibold
+        border border-indigo-200 border-solid border-r-0
+      "
+    >
+      <span>Introduction</span>
+      <span class="text-right">v</span>
+    </div>
+    <div class="pl-8">
+      <div class="border-l border-gray-200 border-solid">
+        <div class="font-bold py-3 px-5 text-gray-400 text-sm">
+          Cheatsheet: Option(in Rust) vs Maybe (in Haskell)
+        </div>
+        <div class="font-bold py-3 px-5 text-gray-400 text-sm">
+          The commands used in Windows and Unix systems
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
