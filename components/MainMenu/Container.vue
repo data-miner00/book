5336b1722aa36496afdcc199f42809d886714581
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-23 03:03:19 +0800

    ✨ Added menu item container

---
+++ components/MainMenu/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex-1 overflow-auto">
+    <div class="h-screen mt-8">
+      <MainMenuSection />
+      <MainMenuSection />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    toggleSubtopic(evt: Event): void {
+      //
+    },
+  },
+})
+</script>
