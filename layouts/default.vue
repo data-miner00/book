183f2a29f58fa241cf951ea8598c75baa4148223
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-23 03:01:16 +0800

    ✨ Added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-row" style="color: #0f1419">
+    <div class="w-1/4 flex-none bg-gray-100">
+      <div class="flex flex-row w-auto justify-end">
+        <div class="w-3/5"><MainMenu /></div>
+      </div>
+    </div>
+    <div class="flex-1"><nuxt /></div>
+  </div>
+</template>
+
+<style lang="postcss">
+.code {
+  @apply font-mono;
+  @apply bg-gray-100;
+  @apply px-2;
+  @apply py-1;
+  @apply rounded;
+}
+</style>
