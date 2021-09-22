7408e724b839afb1ee2277df2fc80ec008659d5f
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-23 03:02:46 +0800

    ✨ Added intro section

---
+++ components/MainMenu/Intro.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="
+      flex flex-row
+      p-5
+      pl-0
+      w-auto
+      justify-between
+      border-solid border-b border-gray-200
+    "
+  >
+    <div class="flex">
+      <!-- secion 1: image -->
+      <div class="h-12 w-12 bg-green-100 mr-4 rounded"></div>
+      <!-- section 2: texts -->
+      <div class="">
+        <div class="text-lg font-bold">Notes</div>
+        <div class="text-xs text-gray-500">My personal take to everything</div>
+      </div>
+    </div>
+
+    <!-- section 3: search-icon -->
+    <div class="ml-auto">x</div>
+  </div>
+</template>
