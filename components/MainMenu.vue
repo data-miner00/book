495633e38bad07c502630d713860157273afc544
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-23 03:02:22 +0800

    ✨ Added main menu

---
+++ components/MainMenu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen border-r border-solid border-gray-200 flex flex-col">
+    <!-- section 1 -->
+    <MainMenuIntro />
+
+    <!-- section 2 -->
+    <MainMenuContainer />
+
+    <!-- section 3 -->
+    <MainMenuFooter />
+  </div>
+</template>
