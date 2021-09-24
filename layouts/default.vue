12656ba01c1411b0a52177950ba4ad28acbb7102
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-24 22:42:51 +0800

    💄 Sticky main menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row" style="color: #0f1419">
     <div class="w-1/4 flex-none bg-gray-100">
-      <div class="flex flex-row w-auto justify-end">
+      <div class="flex flex-row w-auto justify-end sticky top-0">
         <div class="w-3/5"><MainMenu /></div>
       </div>
     </div>
