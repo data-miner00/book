3b84c8e8d933ce493e672772589c8851ca488167
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-24 22:44:48 +0800

    💄 Change font size

--- components/MainMenu/Section.vue
+++ components/MainMenu/Section.vue
@@ -16,10 +16,10 @@
     </div>
     <div class="pl-8">
       <div class="border-l border-gray-200 border-solid">
-        <div class="font-bold py-3 px-5 text-gray-400 text-sm">
+        <div class="font-semibold py-3 px-5 text-gray-400 text-sm">
           Cheatsheet: Option(in Rust) vs Maybe (in Haskell)
         </div>
-        <div class="font-bold py-3 px-5 text-gray-400 text-sm">
+        <div class="font-semibold py-3 px-5 text-gray-400 text-sm">
           The commands used in Windows and Unix systems
         </div>
       </div>
