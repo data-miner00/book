33b6a50430b1f3a7b15946fa4979e6fe638f639d
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-25 12:15:05 +0800

    💄 Better style and expand collapse icon

--- components/MainMenu/Section.vue
+++ components/MainMenu/Section.vue
@@ -9,10 +9,42 @@
         cursor-pointer
         font-semibold
         border border-indigo-200 border-solid border-r-0
+        flex
+        justify-between
+        items-center
       "
     >
-      <span>Introduction</span>
-      <span class="text-right" @click="toggleExpansion">v</span>
+      <div>Introduction</div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-chevron-right"
+        viewBox="0 0 16 16"
+        @click="toggleExpansion"
+        v-if="!isExpanded"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+      <svg
+        v-else
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-chevron-down block"
+        viewBox="0 0 16 16"
+        @click="toggleExpansion"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
     </div>
     <div class="pl-8" v-if="isExpanded">
       <div class="border-l border-gray-200 border-solid">
@@ -44,3 +76,9 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style lang="postcss" scoped>
+.bi {
+  @apply hover:text-purple-500;
+}
+</style>
