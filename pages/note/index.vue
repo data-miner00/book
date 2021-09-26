9f9feb5a962b09198d4309c9ca7289b828ddb091
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-26 21:23:27 +0800

    ✨ Sample menu selection page

---
+++ pages/note/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <ArticleSlot :isMain="true">
+    <div class="max-w-full grid grid-cols-2 gap-4">
+      <div
+        v-for="(a, index) in articles"
+        :key="index"
+        class="
+          this
+          border border-solid border-gray-300
+          p-4
+          shadow-md
+          hover:border-blue-600
+          rounded
+        "
+      >
+        <div class="xmtitle font-semibold">{{ a.title }}</div>
+        <div class="text-sm text-gray-500">{{ a.subtitle }}</div>
+      </div>
+    </div>
+  </ArticleSlot>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {},
+  data: () => {
+    return {
+      articles: [
+        {
+          title: 'Microservice in Go and Multithreading',
+          subtitle: 'A through dive into the technicals',
+        },
+        {
+          title:
+            'Microservice in Go and MultithreadingMicroservice in Go and Multithreading',
+          subtitle: 'A through dive into the technicals',
+        },
+        {
+          title: 'Microservice in Go and Multithreading',
+          subtitle: 'A through dive into the technicals',
+        },
+        {
+          title: 'Microservice in Go and Multithreading',
+          subtitle: 'A through dive into the technicals',
+        },
+        {
+          title: 'Microservice in Go and Multithreading',
+          subtitle: 'A through dive into the technicals',
+        },
+      ],
+    }
+  },
+})
+</script>
+
+<style lang="postcss" scoped>
+.this:hover .xmtitle {
+  @apply text-blue-600;
+}
+</style>
