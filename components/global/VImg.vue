07cf60f0218502f0c13f493d77118601ab236afd
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-26 17:20:32 +0800

    ✨ Added img component

---
+++ components/global/VImg.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img :src="fullpath" :alt="alt" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    src: String,
+    alt: String,
+    caption: String,
+  },
+  computed: {
+    fullpath(): string {
+      return require(`../../assets/images/${this.src}`)
+    },
+  },
+})
+</script>
