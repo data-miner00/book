4e0b29106577fc9094c0f43f5b04fce5fd6d5190
data-miner00 <data-miner00@users.noreply.github.com>
2021-09-28 00:08:58 +0800

    ✨ Checks before render the arrow

--- components/MainMenu/Section.vue
+++ components/MainMenu/Section.vue
@@ -7,51 +7,55 @@
         hover:bg-opacity-10
         cursor-pointer
         font-semibold
-        border border-indigo-200 border-solid border-r-0
         flex
         justify-between
         items-center
       "
+      :class="{
+        'border border-indigo-200 border-solid border-r-0': isExactActive,
+      }"
     >
       <nuxt-link :to="{ name: componentName }" class="flex-1 py-2">{{
         title
       }}</nuxt-link>
 
-      <!-- arrow right icon '>' -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-chevron-right block"
-        viewBox="0 0 16 16"
-        @click="toggleExpansion"
-        v-if="!isExpanded"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-        />
-      </svg>
+      <div v-if="entries.length > 0">
+        <!-- arrow right icon '>' -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-right block"
+          viewBox="0 0 16 16"
+          @click="toggleExpansion"
+          v-if="!isExpanded"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
 
-      <!-- arrow down icon 'v' -->
-      <svg
-        v-else
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-chevron-down block"
-        viewBox="0 0 16 16"
-        @click="toggleExpansion"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-        />
-      </svg>
+        <!-- arrow down icon 'v' -->
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-down block"
+          viewBox="0 0 16 16"
+          @click="toggleExpansion"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
+      </div>
     </div>
-    <div class="pl-8" v-if="isExpanded">
+    <div class="pl-4" v-if="isExpanded">
       <div class="border-l border-gray-200 border-solid">
         <nuxt-link
           v-for="(entry, i) in entries"
