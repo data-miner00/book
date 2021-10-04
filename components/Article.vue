192f967cb1079d0f7cdb2b563f261da64f331608
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-05 00:10:23 +0800

    💬 Updated main page

--- components/Article.vue
+++ components/Article.vue
@@ -2,103 +2,70 @@
   <div class="max-w-screen-md pb-12">
     <div class="py-12 border-b-2 border-gray-200 border-solid mb-12">
       <!-- title  -->
-      <div class="text-4xl font-semibold">Programming</div>
+      <div class="text-4xl font-semibold">Preface</div>
 
       <!-- subtitle -->
       <div class="text-xl mt-2 text-gray-500">
-        The optional subtitle for a page
+        A short introduction to this book 🙂
       </div>
     </div>
 
     <!-- actual content -->
-    <article style="color: rgb(59, 69, 78)" class="leading-7 font-medium">
+    <article style="color: rgb(59, 69, 78)" class="leading-7">
       <p class="mb-6">
-        This is meant to be for people coming from Haskell to Rust or vice versa
-        who want to quickly find the name of corresponding function on optional
-        values. For example, I keep forgetting the names of Rust combinators.
-        Which one I have to use in a particular situation? Is it or_else,
-        <span class="code">unwrap_or</span> orunwrap_or_else? I can imagine that
-        other people may experience similar problems, hence the cheatsheet. You
-        can find examples and more detailed description in the official
-        documentation by clicking on function names.
+        Hi. This is Shaun from <strong>Malaysia</strong> and this notebook
+        consists of various articles and tutorials that I've documented over the
+        years which might be handy for numerous occasions that you might be
+        going though as well. The book is currently under construction so it is
+        expected to have some bugs and misalignement here and there. If you have
+        found an issue, kindly report the issues through the
+        <strong>issues</strong> tab in the [Github
+        Repo](https://github.com/data-miner/book)
       </p>
 
       <p class="mb-4">
-        <span class="font-bold">Update 1:</span> as /u/jkachmar points out on
-        Reddit that there is a note function in some of alternative Preludes in
-        Haskell (or in errors package), which is an analog of ok_or in Rust.
-        Added to the cheatsheet.
+        <span class="font-bold">Incentive:</span> Actually, I have reached out
+        several friends to ask if they are willing to collaborate and contribute
+        to this book about their knowledge but none of them met my expectation,
+        if not, I would prefer building the knowledge repository with them
+        together instead of working as a lone wolf.
       </p>
 
-      <h1 class="text-2xl font-semibold mt-10 mb-4">The title as follows</h1>
+      <h1 class="text-2xl font-semibold mt-10 mb-4">Rule of thumb</h1>
 
-      <p>
-        This is a random <a class="link" href="#">link</a> that can be clicked.
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam,
-        voluptate quidem distinctio aliquam error
-        <a class="link" href="#">nesciunt cumque</a> eius corrupti enim.
-        Aspernatur, fugiat mollitia. Unde consequatur quibusdam expedita at
-        laudantium beatae ad.
+      <p class="my-4">
+        To provide a more intuitive and informative article, there are a few
+        principles that I've always strive to follow.
       </p>
-
-      <h1 class="text-2xl font-semibold pt-10 pb-4">Cheatsheet</h1>
-
-      <table class="w-full">
-        <thead class="text-left border-b-2 border-gray-100 text-gray-400">
-          <tr>
-            <th class="p-1.5">Haskell</th>
-            <th class="p-1.5">Rust</th>
-            <th class="p-1.5">Purpose</th>
-            <th class="p-1.5">Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="border-b border-solid border-gray-100">
-            <td class="p-1.5"><span class="link">Maybe</span></td>
-            <td class="p-1.5"><span class="link">Option</span></td>
-            <td class="p-1.5">type name</td>
-            <td class="p-1.5"></td>
-          </tr>
-          <tr class="border-b border-solid border-gray-100">
-            <td class="p-1.5"><span class="link">Just</span></td>
-            <td class="p-1.5"><span class="link">Some</span></td>
-            <td class="p-1.5">constructor</td>
-            <td class="p-1.5"><span class="code">a -> Maybe a</span></td>
-          </tr>
-          <tr class="border-b border-solid border-gray-100">
-            <td class="p-1.5"><span class="link">Nothing</span></td>
-            <td class="p-1.5"><span class="link">None</span></td>
-            <td class="p-1.5">constructor for no value</td>
-            <td class="p-1.5"><span class="code">Maybe a</span></td>
-          </tr>
-        </tbody>
-      </table>
-
-      <h1 class="text-2xl font-semibold pt-10 pb-4">Bullet points</h1>
 
       <ul class="list-disc list-inside pl-6">
-        <li>item 1</li>
-        <li>item 1</li>
+        <li>Efficient use of tables, diagrams, images and proper wording.</li>
         <li>
-          item 1
-          <ul class="list-disc list-inside pl-6">
-            <li>item item</li>
-            <li>item item</li>
-            <li>item item</li>
-          </ul>
+          Read through the articles again and again, refactoring any inefficient
+          use of wordings
         </li>
-        <li>item 1</li>
-        <li>item 1</li>
-        <li>item 1</li>
+        <li>
+          Research extensively online to get more information on the topic
+        </li>
+      </ul>
+
+      <h1 class="text-2xl font-semibold mt-10 mb-4">Todos</h1>
+
+      <p class="my-4">
+        Although this seems like a very easy project, but the efforts are
+        insurmountable to make it better and better, and hence it takes time and
+        iteration to become fully-functional. Below shows some of the planned
+        features that will be introduced incrementally in my free time.
+      </p>
+
+      <ul class="list-disc list-inside pl-6">
+        <li>Add more articles</li>
+        <li>Add search function</li>
       </ul>
     </article>
 
     <!-- article footer -->
     <footer class="mt-12">
-      <div class="flex gap-1">
-        <Next :isPrev="true" />
-        <Next />
-      </div>
       <div
         class="
           mt-12
@@ -109,7 +76,9 @@
         "
       >
         <div class="w-7 h-7 bg-pink-400 rounded-full mr-4"></div>
-        <div class="text-gray-400 text-xs pt-1">Last updated 1 years ago</div>
+        <div class="text-gray-400 text-xs pt-1">
+          Last updated October 4, 2021
+        </div>
       </div>
     </footer>
   </div>
