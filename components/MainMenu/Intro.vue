db7e3f052b4dadbfc172a1fc35d2662616a7e8ac
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-08 00:38:13 +0800

    💄 Added search button and styles

--- components/MainMenu/Intro.vue
+++ components/MainMenu/Intro.vue
@@ -24,8 +24,37 @@
     </div>
 
     <!-- section 3: search-icon -->
-    <div class="ml-auto flex items-center">
-      <div>✨</div>
+    <div class="ml-auto flex items-center text-gray-500">
+      <div class="search-wrapper">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+          />
+        </svg>
+      </div>
     </div>
   </div>
 </template>
+
+<style lang="sass" scoped>
+.search-wrapper
+  position: relative
+  cursor: pointer
+  &:hover::after
+    content: ''
+    position: absolute
+    top: -10px
+    left: -10px
+    right: -10px
+    bottom: -10px
+    border-radius: 999px
+    background: #e5e5e5
+    z-index: -1
+</style>
