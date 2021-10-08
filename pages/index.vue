3030c2eb3aa5827294e3db49bc122d9bcd5b1c85
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-08 10:58:55 +0800

    ♻️ 💬 Update texts and refactor style class

--- pages/index.vue
+++ pages/index.vue
@@ -8,39 +8,44 @@
 
           <!-- subtitle -->
           <div class="text-xl mt-2 text-gray-500">
-            A short introduction to this book 🙂
+            A short introduction to this digital grimoire 🙂
           </div>
         </div>
 
         <!-- actual content -->
-        <article style="color: rgb(59, 69, 78)" class="leading-7">
-          <p class="mb-6">
-            Hi. This is Shaun from <strong>Malaysia</strong> and this notebook
-            consists of various articles and tutorials that I've documented over
-            the years which might be handy for numerous occasions that you might
-            be going though as well. The book is currently under construction so
-            it is expected to have some bugs and misalignement here and there.
-            If you have found an issue, kindly report the issues through the
+        <article style="color: rgb(59, 69, 78)" class="this leading-7">
+          <p>
+            Hi. This is Shaun from <strong>Malaysia</strong>. Currently learning
+            English and Japanese and this notebook consists of various articles
+            and tutorials that I've documented over the years which might be
+            handy for numerous occasions that you might be going though as well.
+            The book is currently under construction so it is expected to have
+            some bugs and misalignement here and there. If you have found an
+            issue, kindly report the issues through the
             <strong>issues</strong> tab in the [Github
             Repo](https://github.com/data-miner/book)
           </p>
 
-          <p class="mb-4">
-            <span class="font-bold">Incentive:</span> Actually, I have reached
-            out several friends to ask if they are willing to collaborate and
-            contribute to this book about their knowledge but none of them met
-            my expectation, if not, I would prefer building the knowledge
-            repository with them together instead of working as a lone wolf.
+          <p>
+            <strong>Incentive:</strong> Actually, I have reached out several
+            friends to ask if they are willing to collaborate and contribute to
+            this book about their knowledge but none of them met my expectation,
+            if not, I would prefer building the knowledge repository with them
+            together instead of working as a lone wolf.
           </p>
 
-          <h1 class="text-2xl font-semibold mt-10 mb-4">Rule of thumb</h1>
+          <h1>Developed with</h1>
 
-          <p class="my-4">
+          <div></div>
+
+          <h1>Rule of thumb</h1>
+
+          <p>
             To provide a more intuitive and informative article, there are a few
             principles that I've always strive to follow.
           </p>
 
-          <ul class="list-disc list-inside pl-6">
+          <ul>
             <li>
               Efficient use of tables, diagrams, images and proper wording.
             </li>
@@ -53,9 +58,9 @@
             </li>
           </ul>
 
-          <h1 class="text-2xl font-semibold mt-10 mb-4">Todos</h1>
+          <h1>Todos</h1>
 
-          <p class="my-4">
+          <p>
             Although this seems like a very easy project, but the efforts are
             insurmountable to make it better and better, and hence it takes time
             and iteration to become fully-functional. Below shows some of the
@@ -63,7 +68,7 @@
             time.
           </p>
 
-          <ul class="list-disc list-inside pl-6">
+          <ul>
             <li>Add more articles</li>
             <li>Add search function</li>
           </ul>
@@ -119,5 +124,17 @@ export default Vue.extend({
   @apply px-2;
   @apply py-1;
   @apply rounded;
+}
+
+.this p {
+  @apply my-4;
+}
+
+.this h1 {
+  @apply text-2xl font-semibold mt-10 mb-4;
+}
+
+.this ul {
+  @apply list-disc list-inside pl-6;
 }
 </style>
