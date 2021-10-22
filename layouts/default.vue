99560ef901d4c7c728f06e6181ac0ae2a755f71f
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-22 21:07:00 +0800

    📱 Added some responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
       "
     >
       <div class="lg:flex lg:flex-row w-auto lg:justify-end sticky top-0">
-        <div class="lg:w-4/5 2xl:w-3/5"><MainMenu /></div>
+        <div class="xl:w-4/5 2xl:w-3/5 lg:w-full"><MainMenu /></div>
       </div>
     </div>
     <div class="flex-1 flex">
