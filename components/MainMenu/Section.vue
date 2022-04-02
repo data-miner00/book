d2cf43baa8ad3778e639a76fb0515a1f74555154
data-miner00 <data-miner00@users.noreply.github.com>
2022-04-02 16:51:39 +0800

    ♻️ Refactor and fix menu links

--- components/MainMenu/Section.vue
+++ components/MainMenu/Section.vue
@@ -1,76 +1,45 @@
 <template>
   <div>
-    <div
+    <nuxt-link
+      :to="{ name: componentName }"
       class="
-        px-3
+        main
+        px-4
         h-10
         text-gray-700
         hover:bg-opacity-10
         font-semibold
         relative
         flex
+        hover:bg-blue-100 hover:text-blue-600
+        w-full
+        justify-between
+        items-center
       "
-      :class="{
-        'border border-indigo-200 border-solid border-r-0': false,
-      }"
     >
-      <div
-        class="
-          absolute
-          top-0
-          left-0
-          h-full
-          w-full
-          flex
-          justify-between
-          items-center
-          px-4
-          cursor-pointer
-          hover:bg-blue-100 hover:text-blue-600
-        "
-        :class="{ 'bg-blue-100 text-blue-600': thisSection }"
+      <span>{{ title }}</span>
+
+      <button
+        v-if="entries.length > 0"
+        class="absolute top-0 right-4 transition-transform duration-200 p-3"
+        @click.prevent.stop="toggleExpansion"
+        :class="{ 'transform rotate-90': isExpanded }"
       >
-        <nuxt-link :to="{ name: componentName }" class="flex-1 py-2">{{
-          title
-        }}</nuxt-link>
-
-        <div v-if="entries.length > 0">
-          <!-- arrow right icon '>' -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-chevron-right block"
-            viewBox="0 0 16 16"
-            @click="toggleExpansion"
-            v-if="!isExpanded"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-            />
-          </svg>
-
-          <!-- arrow down icon 'v' -->
-          <svg
-            v-else
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-chevron-down block"
-            viewBox="0 0 16 16"
-            @click="toggleExpansion"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-            />
-          </svg>
-        </div>
-      </div>
-    </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-right block"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
+      </button>
+    </nuxt-link>
     <div class="pl-4" v-if="isExpanded">
       <div class="border-l border-gray-200 border-solid">
         <nuxt-link
@@ -119,6 +88,7 @@ export default Vue.extend({
     fullComponentName(): string {
       return `${this.componentName}-slug`
     },
+    // Not used
     thisSection(): boolean {
       return this.$route.name == this.fullComponentName
     },
@@ -133,5 +103,9 @@ export default Vue.extend({
 
 .active {
   @apply bg-white hover:bg-white bg-opacity-70 text-purple-600;
+}
+
+.main.nuxt-link-exact-active {
+  @apply bg-blue-100 text-blue-600;
 }
 </style>
