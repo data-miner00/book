33b4695fce0b2c89b954a6ab1750328d48ce76dc
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-15 23:48:33 +0800

    fix: Math block svg issues

--- layouts/default.vue
+++ layouts/default.vue
@@ -175,7 +175,11 @@ export default Vue.extend({
   @apply border-b border-solid border-gray-100;
 }
 
-.nuxt-content mjx-container svg {
+.nuxt-content .math-inline mjx-container svg {
+  display: inline;
+}
+
+.nuxt-content .math:not(.math-inline) mjx-container svg {
   /* height: 4ex; */
   display: block;
   margin: 0 auto;
