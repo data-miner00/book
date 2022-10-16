344c63f760dd7f446091da398bd398f8463e8999
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-16 21:08:58 +0800

    fix: Suppress unknown mjx-container error

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,4 +62,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  // Vue config
+  vue: {
+    config: {
+      ignoredElements: ['mjx-container'],
+    },
+  },
 }
