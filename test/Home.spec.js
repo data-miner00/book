fe3d4acce30f2665afd8849cf73bc93e601e7fd4
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-19 20:17:45 +0800

    🔥 Remove useless unit test

--- test/Home.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Home from '../pages/index.vue'
-
-describe('Home page', () => {
-  it('is a Vue instance', () => {
-    const wrapper = mount(Home)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
