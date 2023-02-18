18dc77150f1a924f9e18f702fc17cf1dd5337302
data-miner00 <data-miner00@users.noreply.github.com>
2023-02-18 18:03:27 +0800

    ✨ State to keep track sidebar open for mobile

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,22 @@
 export const state = () => ({
   isSearchPanelOpen: false,
+  isSidebarOpen: true,
 })
 
 export const mutations = {
   toggleSearchPanel(state: any) {
     state.isSearchPanelOpen = !state.isSearchPanelOpen
   },
+  toggleSidebar(state: any) {
+    state.isSidebarOpen = !state.isSidebarOpen
+  },
 }
 
 export const getters = {
   getSearchPanelState(state: any) {
     return state.isSearchPanelOpen
+  },
+  getSidebarState(state: any) {
+    return state.isSidebarOpen
   },
 }
