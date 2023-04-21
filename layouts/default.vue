9cf14c5fa5e350dbe8065086138254ae8f5d495d
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-22 00:53:13 +0800

    🚑 Use global css to load font

--- layouts/default.vue
+++ layouts/default.vue
@@ -78,8 +78,6 @@ export default Vue.extend({
 </script>
 
 <style lang="postcss">
-@import url('https://fonts.googleapis.com/css2?family=Source+Serif+Pro:ital,wght@0,300;0,400;0,600;0,700;1,400;1,600;1,700&display=swap');
-
 .nuxt-content {
   font-family: 'Source Serif Pro', serif;
 }
