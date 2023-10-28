4e97839259b383eeca53826394734c4644b4c33d
data-miner00 <data-miner00@users.noreply.github.com>
2023-10-28 21:00:31 +0800

    🎨 Fix weird dangling svg tag

--- components/Tags.vue
+++ components/Tags.vue
@@ -14,8 +14,9 @@
         />
         <path
           d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z"
-        /></svg
-      ><span class="ml-1">Tags</span>
+        />
+      </svg>
+      <span class="ml-1">Tags</span>
     </div>
     <div class="flex mt-3 gap-1 flex-wrap">
       <div
